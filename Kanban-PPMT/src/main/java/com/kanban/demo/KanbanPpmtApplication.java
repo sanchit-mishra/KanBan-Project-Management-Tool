87e8006500a40f0b4a075265d67df95975b2d467
unknown <sanchitmishra88@gmail.com>
2020-07-08 09:38:40 +0530

    First Commit

---
+++ Kanban-PPMT/src/main/java/com/kanban/demo/KanbanPpmtApplication.java
@@ -0,0 +1,13 @@
+package com.kanban.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanbanPpmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanbanPpmtApplication.class, args);
+	}
+
+}
