6b2d35ae57dfb177311acb23aa8df629f9ff4cb6
unknown <sanchitmishra88@gmail.com>
2020-07-10 02:31:15 +0530

    Exception Handling

--- Kanban-PPMT/src/main/java/com/kanban/demo/repository/ProjectRepository.java
+++ Kanban-PPMT/src/main/java/com/kanban/demo/repository/ProjectRepository.java
@@ -8,7 +8,6 @@ import com.kanban.demo.domain.Project;
 @Repository
 public interface ProjectRepository extends CrudRepository<Project, Long> {
 
-	@Override
-	Iterable<Project> findAllById(Iterable<Long> ids);
+	Project findByProjectIdentifier(String projectIdentifier);
 	
 }
--- Kanban-PPMT/src/main/java/com/kanban/demo/services/ProjectService.java
+++ Kanban-PPMT/src/main/java/com/kanban/demo/services/ProjectService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.kanban.demo.domain.Project;
+import com.kanban.demo.exceptions.ProjectIdException;
 import com.kanban.demo.repository.ProjectRepository;
 
 @Service
@@ -14,9 +15,22 @@ public class ProjectService {
 	
 	
 	public Project saveOrUpdate(Project project) {
+		try {
+			project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+			return projectRepository.save(project);
+		}
+		catch(Exception ex) {
+			throw new ProjectIdException("Project Identifier "+ project.getProjectIdentifier().toUpperCase() +" is already taken");
+		}
+	
+	}
+	
+	public Project findProjectByIdentifier(String projectId) {
+		Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
 		
-		//Logic
-		
-		return projectRepository.save(project);
+		if(project == null) {
+			throw new ProjectIdException("Project ID"+ projectId +" does not exists");
+		}
+		return project;
 	}
 }
