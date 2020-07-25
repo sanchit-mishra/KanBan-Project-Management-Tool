fe2df7e57ceef52119dc6f9edb384e173b144f3b
unknown <sanchitmishra88@gmail.com>
2020-07-25 21:42:06 +0530

    Project Board

--- kanban-ppmt-react/src/reducers/index.js
+++ kanban-ppmt-react/src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 import errorReducer from "./errorReducer";
 import projectReducer from "./projectReducer";
+import backlogReducer from "./backlogReducer";
 
 //Reducer State
 export default combineReducers({
   errors: errorReducer,
   project: projectReducer,
+  backlog: backlogReducer,
 });
