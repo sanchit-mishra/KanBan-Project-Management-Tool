912671287d083f5b6e5eeecee22cafaee1ff7a9c
unknown <sanchitmishra88@gmail.com>
2020-07-25 22:35:35 +0530

    Bug Fixes for ProjectAction res Line 6

--- kanban-ppmt-react/src/components/ProjectBoard/ProjectTasks/AddProjectTask.js
+++ kanban-ppmt-react/src/components/ProjectBoard/ProjectTasks/AddProjectTask.js
@@ -1,43 +1,51 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class AddProjectTask extends Component {
   render() {
+    const { id } = this.props.match.params;
     return (
-      <div class="add-PBI">
-        <div class="container">
-          <div class="row">
-            <div class="col-md-8 m-auto">
-              <a href="#" class="btn btn-light">
+      <div className="add-PBI">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <Link
+                to={`/projectBoard/${id}`}
+                className="btn bg-secondary btn-light "
+              >
                 Back to Project Board
-              </a>
-              <h4 class="display-4 text-center">Add /Update Project Task</h4>
-              <p class="lead text-center">Project Name + Project Code</p>
+              </Link>
+              <h4 className="display-4 text-center">Add Project Task</h4>
+              <p className="lead text-center">Project Name + Project Code</p>
               <form>
-                <div class="form-group">
+                <div className="form-group">
                   <input
                     type="text"
-                    class="form-control form-control-lg"
+                    className="form-control form-control-lg"
                     name="summary"
                     placeholder="Project Task summary"
                   />
                 </div>
-                <div class="form-group">
+                <div className="form-group">
                   <textarea
-                    class="form-control form-control-lg"
+                    className="form-control form-control-lg"
                     placeholder="Acceptance Criteria"
                     name="acceptanceCriteria"
                   ></textarea>
                 </div>
                 <h6>Due Date</h6>
-                <div class="form-group">
+                <div className="form-group">
                   <input
                     type="date"
-                    class="form-control form-control-lg"
+                    className="form-control form-control-lg"
                     name="dueDate"
                   />
                 </div>
-                <div class="form-group">
-                  <select class="form-control form-control-lg" name="priority">
+                <div className="form-group">
+                  <select
+                    className="form-control form-control-lg"
+                    name="priority"
+                  >
                     <option value={0}>Select Priority</option>
                     <option value={1}>High</option>
                     <option value={2}>Medium</option>
@@ -45,8 +53,11 @@ class AddProjectTask extends Component {
                   </select>
                 </div>
 
-                <div class="form-group">
-                  <select class="form-control form-control-lg" name="status">
+                <div className="form-group">
+                  <select
+                    className="form-control form-control-lg"
+                    name="status"
+                  >
                     <option value="">Select Status</option>
                     <option value="TO_DO">TO DO</option>
                     <option value="IN_PROGRESS">IN PROGRESS</option>
@@ -54,7 +65,10 @@ class AddProjectTask extends Component {
                   </select>
                 </div>
 
-                <input type="submit" class="btn btn-primary btn-block mt-4" />
+                <input
+                  type="submit"
+                  className="btn btn-primary btn-block mt-4"
+                />
               </form>
             </div>
           </div>
