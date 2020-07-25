ca0f5953953f13ae662640a969835c69f81f27a8
unknown <sanchitmishra88@gmail.com>
2020-07-25 22:18:58 +0530

    AddProjectTask Form

--- kanban-ppmt-react/src/App.js
+++ kanban-ppmt-react/src/App.js
@@ -9,6 +9,7 @@ import { Provider } from "react-redux";
 import store from "./store";
 import UpdateProject from "./components/Project/UpdateProject";
 import ProjectBoard from "./components/ProjectBoard/ProjectBoard";
+import AddProjectTask from "./components/ProjectBoard/ProjectTasks/AddProjectTask";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
           <Route exact path="/addProject" component={AddProject} />
           <Route exact path="/updateProject/:id" component={UpdateProject} />
           <Route exact path="/projectBoard/:id" component={ProjectBoard} />
+          <Route exact path="/addProjectTask/:id" component={AddProjectTask} />
         </div>
       </Router>
     </Provider>
